45ca491810392d6a8c4309674fa2b48002c0c49e
Crowdbotics <team@crowdbotics.com>
2020-09-28 22:40:12 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -6,7 +6,6 @@ export const installed_blueprints = [
 { name: 'Tutorial4114759', human_name: 'Tutorial 4', access_route: 'Tutorial4114759', icon: 'question-circle'},
 { name: 'SignUp25114758', human_name: 'Sign Up 2 5', access_route: 'SignUp25114758'},
 { name: 'BlankScreen2114757', human_name: 'Blank Screen 2', access_route: 'BlankScreen2114757'},
-{ name: 'BlankScreen1114756', human_name: 'Blank Screen 1', access_route: 'BlankScreen1114756'},
 { name: 'BlankScreen0114755', human_name: 'Blank Screen 0', access_route: 'BlankScreen0114755'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import SignUp26114763Navigator from '../features/SignUp26114763/navigator';
 import Tutorial4114759Navigator from '../features/Tutorial4114759/navigator';
 import SignUp25114758Navigator from '../features/SignUp25114758/navigator';
 import BlankScreen2114757Navigator from '../features/BlankScreen2114757/navigator';
-import BlankScreen1114756Navigator from '../features/BlankScreen1114756/navigator';
 import BlankScreen0114755Navigator from '../features/BlankScreen0114755/navigator';
 
 /**
@@ -25,7 +24,6 @@ SignUp26114763: { screen: SignUp26114763Navigator },
 Tutorial4114759: { screen: Tutorial4114759Navigator },
 SignUp25114758: { screen: SignUp25114758Navigator },
 BlankScreen2114757: { screen: BlankScreen2114757Navigator },
-BlankScreen1114756: { screen: BlankScreen1114756Navigator },
 BlankScreen0114755: { screen: BlankScreen0114755Navigator },
 
     /** new navigators can be added here */
